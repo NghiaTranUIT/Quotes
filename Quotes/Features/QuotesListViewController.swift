0c14d17e6d80078feca3bf516661793d0e379cd7
Tomasz Szulc <mail@szulctomasz.com>
2015-07-08 23:14:51 +0200

    Deselect cell with animation

--- Quotes/Features/QuotesListViewController.swift
+++ Quotes/Features/QuotesListViewController.swift
@@ -38,7 +38,7 @@ class QuotesListViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
